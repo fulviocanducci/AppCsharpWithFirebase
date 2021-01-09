35b9a2dcb23267f98387e36872a31a1246ebcac4
fulviocanducci <fulviocanducci@hotmail.com>
2021-01-08 21:32:30 -0300

    Add project files.

---
+++ AppCsharpWithFirebase/Models/Peoples.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AppCsharpWithFirebase.Models
+{
+    public class Peoples
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+
+        public Peoples(Guid id, string name, DateTime createdAt)
+        {
+            Id = id;
+            Name = name;
+            CreatedAt = createdAt;
+        }
+
+        public Peoples(string name, DateTime createdAt)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            CreatedAt = createdAt;
+        }
+
+        public Peoples(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            CreatedAt = DateTime.Now;
+        }
+        public Peoples()
+        {
+
+        }
+    }
+}
---
+++ AppCsharpWithFirebase/Program.cs
@@ -0,0 +1,46 @@
+﻿using AppCsharpWithFirebase.Models;
+using Firebase.Database;
+using System;
+using System.Text.Json;
+
+namespace AppCsharpWithFirebase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string db = "Database/Peoples";
+            FirebaseClient firebase =
+                new FirebaseClient("https://csharp-localhost-default-rtdb.firebaseio.com/");
+
+            Peoples peoples =
+                new Peoples("Gabriel Santos de Andrade", DateTime.Now);
+
+            // ADD
+            //var result = firebase.Child(db)
+            //    .PostAsync(JsonSerializer.Serialize(peoples))
+            //    .GetAwaiter()
+            //    .GetResult();
+
+            // UPDATE - -MQZMuByH5vCRx_4QHBs
+            //firebase.Child($"{db}/{"-MQZN6qN-A4EvkzQuCa0"}" )                
+            //    .PutAsync(JsonSerializer.Serialize(peoples))
+            //    .GetAwaiter()
+            //    .GetResult();
+
+
+
+            // LIST            
+            var list = firebase.Child(db)
+                .OnceAsync<Peoples>()
+                .GetAwaiter()
+                .GetResult();
+
+            foreach(var item in list)
+            {
+                System.Console.WriteLine("{0} - {1} {2}", item.Key, item.Object.Name, item.Object.CreatedAt);
+            }
+
+        }
+    }
+}
